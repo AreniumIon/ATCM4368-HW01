00b4003011e500668b768cba0d34470436a28d80
AreniumIon <jeffreydickenson9@gmail.com>
2021-08-27 16:01:36 -0500

    Inheritance setup doc walkthrough

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int _damageAmount = 1;
+    [SerializeField] ParticleSystem _impactParticles;
+    [SerializeField] AudioClip _impactSound;
+
+    Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            PlayerImpact(player);
+            ImpactFeedback();
+        }
+    }
+
+    protected virtual void PlayerImpact(Player player)
+    {
+        player.DecreaseHealth(_damageAmount);
+    }
+
+    private void ImpactFeedback()
+    {
+        // Particles
+        if (_impactParticles != null)
+        {
+            _impactParticles = Instantiate(_impactParticles, transform.position, Quaternion.identity);
+        }
+        // Audio
+        if (_impactSound != null)
+        {
+            AudioHelper.PlayClip2D(_impactSound, 1f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+
+    }
+}
